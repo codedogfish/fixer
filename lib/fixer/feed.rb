ed4026bee84ce378958cec35435e608d84e51329
JackYu <jackcool1989@outlook.com>
2019-01-02 18:23:46 +0800

    fix fixer :P

---
+++ lib/fixer/feed.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'rexml/document'
+
+module Fixer
+  # Wraps ECB's data feed
+  class Feed
+    include Enumerable
+
+    SCOPES = {
+      current: 'daily',
+      ninety_days: 'hist-90d',
+      historical: 'hist-90d'
+    }.freeze
+
+    def initialize(scope = :current)
+      @scope = SCOPES.fetch(scope) { raise ArgumentError }
+    end
+
+    def each
+      REXML::XPath.each(document, '/gesmes:Envelope/Cube/Cube[@time]') do |day|
+        date = Date.parse(day.attribute('time').value)
+        REXML::XPath.each(day, './Cube') do |currency|
+          yield(
+            date: date,
+            iso_code: currency.attribute('currency').value,
+            rate: Float(currency.attribute('rate').value)
+          )
+        end
+      end
+    end
+
+    private
+
+    def document
+      puts xml
+      REXML::Document.new(xml)
+    end
+
+    def xml
+      Net::HTTP.get(url)
+    end
+
+    def url
+      puts("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-#{@scope}.xml")
+      URI("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-#{@scope}.xml")
+    end
+  end
+end
